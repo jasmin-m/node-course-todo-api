63ec0795c6ffff0502f640e66736cd9bac05fb81
Jasmin <jasmin-m@users.noreply.github.com>
2018-01-23 10:47:47 +1100

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -41,35 +41,35 @@ app.get('/todos/:id', (req, res) => {
   }
 
   Todo.findById(id).then((todo) => {
-      if(!todo){
-        return res.status(404).send();
-      }
+    if(!todo){
+      return res.status(404).send();
+    }
 
-      res.send({todo});
-    }).catch((e) => {
-      res.status(400).send();
-    });
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.delete('/todos/:id', (req, res) => {
-  //Get the id
   var id = req.params.id;
-  //validate the id -> not valid? return 404
+
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
-  //remove todo by id
+
   Todo.findByIdAndRemove(id).then((todo) => {
     if(!todo){
       return res.status(404).send();
     }
-    res.status(200).send(todo);
-    }).catch((e) => {
-      res.status(400).send();
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
-
-app.listen(port, () => {
-  console.log(`Starting on port ${port}`);
 });
 
-module.exports = {app};
+  app.listen(port, () => {
+    console.log(`Starting on port ${port}`);
+  });
+
+  module.exports = {app};
